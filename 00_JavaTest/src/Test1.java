e1322cd3dc4fade349b7fd257411030a33a17687
gunam-kim <gunam90@naver.com>
2025-01-20 11:25:24 +0900

    Java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,22 @@
+/* Run 버튼 (단축키 ctrl + F11)
+ * 1) 자동으로 컴파일 수행 (javac xxx.java)
+ * -> .class 파일 생성
+ * 2) 컴파일 완료 후 .class 파일 실행 (java class파일명)
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("이클립스 실행중");
+		System.out.println("Eclipse");
+
+		/* Java API DOC
+		 * : Java에서 제공해주는 코드의 사용법이 설명되어있는 문서
+		 * 
+		 * DOC 확인 방법
+		 * 	1) google에서 java 21 API 검색
+		 * 	2) 궁금한 코드 클릭하고 shift + F2
+		 */
+	}
+
+}
